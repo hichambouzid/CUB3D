b1d5a029d2b859777d2eb47f63d112e092ffa2c6
hichambouzid <bouzidhicham51@gmail.com>
2024-07-08 16:56:07 +0100

    PUSH-H-0.0.2: udpat  main.c

--- cub3D.h
+++ cub3D.h
@@ -1,4 +1,13 @@
 #ifndef CUB3D_H
 #define CUB3D_H
 
+#include <mlx.h>
+
+typedef struct data
+{
+	void *mlx;
+	void *mlx_win;
+
+} t_data;
+void init_window(t_data *data);
 #endif
--- main.c
+++ main.c
@@ -6,9 +6,47 @@
 /*   By: hibouzid <hibouzid@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:55:30 by hibouzid          #+#    #+#             */
-/*   Updated: 2024/07/06 18:55:47 by hibouzid         ###   ########.fr       */
+/*   Updated: 2024/07/08 16:55:26 by hibouzid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
+void ft_putstr_fd(char *str, int fd)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		write(fd, &str[i++], 1);
+}
+
+void init_window(t_data *data)
+{
+	data->mlx = mlx_init();
+	if (!data->mlx)
+	{
+		ft_putstr_fd(2, "error: canno't initializ windows\n");
+		exit(1);
+	}
+	data->mlx_win = mlx_new_window(data->mlx, 1920, 1080, "CUB3D");
+	mlx_loop(data->mlx);
+}
+
+void processInput()
+{
+	// listing to ky board
+}
+
+int main(int ac, char **av)
+{
+	t_data data;
+	
+	init_window(&data);
+	while (1)
+	{
+		proccesInput();
+		Updat();
+		runder();
+	}
+}
